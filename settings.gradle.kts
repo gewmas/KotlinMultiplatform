d8149b3c5ef7700aa193da4eab893ea1f098a24f
Yuhua Mai <yuhua.mai@airbnb.com>
2023-09-30 20:42:07 -0700

    Create your first cross-platform app﻿

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KotlinMultiplatform"
+include(":androidApp")
+include(":shared")
